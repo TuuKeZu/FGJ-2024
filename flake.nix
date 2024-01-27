ee4ce174c817337322272d753e2cd27e59d6c21b
Matias Zwinger <matias.zwinger@protonmail.com>
2024-01-27 16:01:41 +0200

    Fix asset filter and clean up flake

--- flake.nix
+++ flake.nix
@@ -23,29 +23,45 @@
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs.lib) hasInfix hasSuffix;
 
-      assetFilter = path: _type: baseNameOf path != toString ./assets;
-      customFitler = path: type: (assetFilter path type || craneLib.filterCargoSources path type);
+      pkgs = nixpkgs.legacyPackages.${system};
       craneLib = crane.lib.${system};
+
+      buildInputs = with pkgs; [
+        vulkan-loader
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        alsa-lib
+        udev
+      ];
+
       fgc_2024 = craneLib.buildPackage {
+        inherit buildInputs;
         src = pkgs.lib.cleanSourceWith {
-          src = craneLib.path ./.;
-          filter = customFitler;
+          src = ./.;
+          filter = path: type:
+            (
+              hasInfix "/assets/" path
+              && (
+                hasSuffix ".png" path
+                || hasSuffix ".ttf" path
+                || hasSuffix ".json" path
+                || hasSuffix ".csv" path
+              )
+            )
+            || (craneLib.filterCargoSources path type);
         };
         strictDeps = true;
         cargoExtraArgs = "--no-default-features";
+        installPhaseCommand = ''
+          mkdir -p $out/bin
+          cp -r assets $out/bin
+          installFromCargoBuildLog "$out" "$cargoBuildLog"
+        '';
 
         nativeBuildInputs = [pkgs.pkg-config];
-        buildInputs = with pkgs; [
-          vulkan-loader
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          alsa-lib
-          udev
-        ];
-
         # Additional environment variables can be set directly
         # MY_CUSTOM_VAR = "some value";
       };
@@ -75,74 +91,62 @@
         drv = fgc_2024;
       };
 
-      devShells.default = let
-        deps = with pkgs; [
-          vulkan-loader
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          alsa-lib
-          udev
-          libxkbcommon
-          wayland
-        ];
-      in
-        devenv.lib.mkShell {
-          # Inherit inputs from checks.
-          inherit inputs pkgs;
+      devShells.default = devenv.lib.mkShell {
+        # Inherit inputs from checks.
+        inherit inputs pkgs;
 
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          modules = [
-            ({
-              pkgs,
-              config,
-              ...
-            }: {
-              packages = with pkgs; [
-                pre-commit
-                trunk
-                watch_tool
-              ];
+        # Extra inputs can be added here; cargo and rustc are provided by default.
+        modules = [
+          ({
+            pkgs,
+            config,
+            ...
+          }: {
+            packages = with pkgs; [
+              pre-commit
+              trunk
+              watch_tool
+            ];
 
-              # FIXME
-              pre-commit.default_stages = ["manual"];
-              # current workaround:
-              # 1. remove .pre-commit-config.yaml but don't stage the deletion
-              # 2. run `pre-commit run --all-files --hook-stage manual` before every commit
+            # gona
+            env.LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
 
-              pre-commit.hooks = {
-                # TODO cargo check
-                rustfmt.enable = true;
-                alejandra.enable = true;
-              };
+            # FIXME
+            pre-commit.default_stages = ["manual"];
+            # current workaround:
+            # 1. remove .pre-commit-config.yaml but don't stage the deletion
+            # 2. run `pre-commit run --all-files --hook-stage manual` before every commit
 
-              scripts.cargo-watch.exec = ''
-                RED='\033[0;31m'
-                GREEN='\033[0;32m'
-                BLUE='\033[0;34m'
-                NC='\033[0m'
+            pre-commit.hooks = {
+              # TODO cargo check
+              rustfmt.enable = true;
+              alejandra.enable = true;
+            };
 
-                build () {
-                  if nix build .# -L; then
-                    echo -e "''${GREEN}Build successful''${NC}";
-                  else
-                    echo -e "''${RED}Build failed''${NC}";
-                  fi
-                }
+            scripts.cargo-watch.exec = ''
+              RED='\033[0;31m'
+              GREEN='\033[0;32m'
+              BLUE='\033[0;34m'
+              NC='\033[0m'
 
+              build () {
+                if nix build .# -L; then
+                  echo -e "''${GREEN}Build successful''${NC}";
+                else
+                  echo -e "''${RED}Build failed''${NC}";
+                fi
+              }
+
+              build
+              echo -e "''${BLUE}Watching for changes''${NC}"
+
+              ${pkgs.inotify-tools}/bin/inotifywait -q -e close_write,moved_to,create -r -m ./src |
+              while read -r directory events filename; do
                 build
-                echo -e "''${BLUE}Watching for changes''${NC}"
-
-                ${pkgs.inotify-tools}/bin/inotifywait -q -e close_write,moved_to,create -r -m ./src |
-                while read -r directory events filename; do
-                  build
-                done
-              '';
-
-              # gona
-              env.LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath deps}";
-            })
-          ];
-        };
+              done
+            '';
+          })
+        ];
+      };
     });
 }

e5acf988456cc876cdbb2ebf51dc342b79816061
TuuKeZu <moilanentuukka5@gmail.com>
2024-01-28 12:58:12 +0200

    fix pointer

--- src/pointer.rs
+++ src/pointer.rs
@@ -18,6 +18,7 @@ impl PointerBundle {
         Self {
             pointer: Pointer {},
             sprite_bundle: SpriteBundle {
+                transform: Transform::from_translation(Vec3::new(0., 0., 100.)),
                 sprite: Sprite {
                     color: Color::rgb(255., 0., 0.),
                     custom_size: Some(Vec2::new(2. * 5., 2. * 20.)),
@@ -41,7 +42,8 @@ pub fn handle_pointer(
     let mut pointer_transform = pointer_q.get_single_mut().unwrap();
     let (car_transform, _) = car_q.get_single().unwrap();
 
-    pointer_transform.translation = car_transform.translation;
+    pointer_transform.translation.x = car_transform.translation.x;
+    pointer_transform.translation.y = car_transform.translation.y;
 
     if let Ok((target_transform, _)) = target_q.get_single() {
         let delta = target_transform.translation - pointer_transform.translation;
